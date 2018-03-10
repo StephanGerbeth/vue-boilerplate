9d1144e52f8c3ac15390d4b7132b8c440a890d10
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-03-10 10:14:28 +0100

    fix(saucelabs): added log

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -3,6 +3,7 @@ var webdriver = require('selenium-webdriver');
 
 describe('testing javascript in the browser', function () {
   beforeEach(function () {
+    console.log(process.env.SAUCE_USERNAME);
     if (process.env.SAUCE_USERNAME !== undefined) {
       this.browser = new webdriver.Builder()
         .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
