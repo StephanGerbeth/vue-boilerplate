e37fa0021388c0536cc12b9d2fe07cf55a7a73f1
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-03-10 10:24:25 +0100

    fix(saucelabs): updated test routine

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -3,7 +3,6 @@ var webdriver = require('selenium-webdriver');
 
 describe('testing javascript in the browser', function () {
   beforeEach(function () {
-    console.log(process.env.SAUCE_USERNAME);
     if (process.env.SAUCE_USERNAME !== undefined) {
       this.browser = new webdriver.Builder()
         .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
@@ -21,7 +20,7 @@ describe('testing javascript in the browser', function () {
         }).build();
     }
 
-    return this.browser.get('http://localhost:8050/index.html');
+    return this.browser.get('http://localhost:8050/de');
   });
 
   afterEach(function () {
@@ -30,11 +29,10 @@ describe('testing javascript in the browser', function () {
 
   it('should handle clicking on a headline', function (done) {
     var headline = this.browser.findElement(webdriver.By.css('h1'));
-
     headline.click();
 
     headline.getText().then(function (txt) {
-      assert.equal(txt, 'awesome');
+      assert.equal(txt, 'Headline');
       done();
     });
   });
