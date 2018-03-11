564f4080534319f6bca048ee027c12a6dfda39af
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-03-11 10:17:13 +0100

    fix(saucelab): multi browser test

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -1,55 +1,39 @@
-// var assert = require('assert');
+const {getBrowser} = require('./saucelabs/setup');
 var webdriver = require('selenium-webdriver');
-// var b2s = require('saucelabs-multicapabilities-generator');
-// var fs = require('fs');
 
-describe('testing javascript in the browser', function () {
-  beforeEach(function () {
-    if (process.env.SAUCE_USERNAME !== undefined) {
-      this.browser = new webdriver.Builder()
-        .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
-        .withCapabilities({
-          'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-          build: process.env.TRAVIS_BUILD_NUMBER,
-          username: process.env.SAUCE_USERNAME,
-          accessKey: process.env.SAUCE_ACCESS_KEY,
-          browserName: 'chrome'
-        }).withCapabilities({
-          'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-          build: process.env.TRAVIS_BUILD_NUMBER,
-          username: process.env.SAUCE_USERNAME,
-          accessKey: process.env.SAUCE_ACCESS_KEY,
-          browserName: 'firefox'
-        }).build();
-    } else {
-      this.browser = new webdriver.Builder()
-        .withCapabilities({
-          browserName: 'chrome'
-        }).build();
-    }
-    //
-    // let list = fs.readFileSync('./.browserslistrc').toString().trim().split(/[\r\n]+/m);
-    // console.log('LIST', list);
-    // console.log(b2s(list).then(function (result) {
-    //   console.log(result);
-    // }));
-    // // console.log(b2s({ browsers: ['> 1% in DE'] }));
+let capabilities = [{browserName: 'firefox'}, {browserName: 'chrome'}];
 
-    return this.browser.get('http://localhost:8050/de/about');
-  });
+for (let capability in capabilities) {
+  describe('testing javascript in the browser', function () {
+    before(function () {
+      this.browser = getBrowser(capabilities[capability]);
+      this.browser.get('http://localhost:8050/de/about');
+    });
 
-  afterEach(function () {
-    return this.browser.quit();
-  });
+    after(function (...args) {
+      console.log(args);
+      return this.browser.quit();
+    });
 
-  it('should handle clicking on a headline', function (done) {
-    var headline = this.browser.findElement(webdriver.By.css('h1'));
-    // headline.click();
+    it('should handle clicking on a headline', function (done) {
+      var headline = this.browser.findElement(webdriver.By.css('h1'));
+      // headline.click();
 
-    headline.getText().then(function (txt) {
-      console.log(txt);
-      // assert.equal(txt, 'Headline');
-      done();
+      headline.getText().then(function (txt) {
+        console.log(txt);
+        // assert.equal(txt, 'Headline');
+        done();
+      });
+    });
+    it('kaboom', function (done) {
+      var headline = this.browser.findElement(webdriver.By.css('h1'));
+      // headline.click();
+
+      headline.getText().then(function (txt) {
+        console.log(txt);
+        // assert.equal(txt, 'Headline');
+        done();
+      });
     });
   });
-});
+}
