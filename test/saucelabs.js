17a8c63b1b05ebc8eea7c9ee3fe3d42895a573cc
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-03-11 08:02:33 +0100

    fix(saucelab): test multi browser

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -1,5 +1,7 @@
 // var assert = require('assert');
 var webdriver = require('selenium-webdriver');
+// var b2s = require('saucelabs-multicapabilities-generator');
+// var fs = require('fs');
 
 describe('testing javascript in the browser', function () {
   beforeEach(function () {
@@ -12,6 +14,12 @@ describe('testing javascript in the browser', function () {
           username: process.env.SAUCE_USERNAME,
           accessKey: process.env.SAUCE_ACCESS_KEY,
           browserName: 'chrome'
+        }).withCapabilities({
+          'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+          build: process.env.TRAVIS_BUILD_NUMBER,
+          username: process.env.SAUCE_USERNAME,
+          accessKey: process.env.SAUCE_ACCESS_KEY,
+          browserName: 'firefox'
         }).build();
     } else {
       this.browser = new webdriver.Builder()
@@ -19,6 +27,14 @@ describe('testing javascript in the browser', function () {
           browserName: 'chrome'
         }).build();
     }
+    //
+    // let list = fs.readFileSync('./.browserslistrc').toString().trim().split(/[\r\n]+/m);
+    // console.log('LIST', list);
+    // console.log(b2s(list).then(function (result) {
+    //   console.log(result);
+    // }));
+    // // console.log(b2s({ browsers: ['> 1% in DE'] }));
+
     return this.browser.get('http://localhost:8050/de/about');
   });
 
