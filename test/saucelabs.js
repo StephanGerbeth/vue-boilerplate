5117194d66b9acb33418a873fab3478a49bc2b65
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2018-03-11 14:01:01 +0100

    feat(saucelabs): final sample

--- test/saucelabs.js
+++ test/saucelabs.js
@@ -1,40 +1,27 @@
 const {getBrowser} = require('./saucelabs/setup');
-var webdriver = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver');
 
 let capabilities = [{browserName: 'firefox'}, {browserName: 'chrome'}];
 
 for (let capability in capabilities) {
   describe('open browser:' + capabilities[capability].browserName, function () {
+    let browser = null;
+
     before(function () {
-      this.browser = getBrowser(capabilities[capability]);
-      this.browser.get('http://localhost:8050/de/about');
+      browser = getBrowser(capabilities[capability]);
+      browser.get('http://localhost:8050/de/about');
     });
 
-    after(function (...args) {
-      console.log('quit browser:', capabilities[capability].browserName);
-      return this.browser.quit();
+    after(function () {
+      return browser.quit();
     });
 
-    it('should handle clicking on a headline', function (done) {
-      this.browser.wait(webdriver.until.elementLocated(webdriver.By.css('h1')))
-        .then(el => {
-          return el.getText();
-        })
-        .then(txt => {
-          console.log(txt);
-          done();
-        });
-    });
-
-    it('should handle clicking on a headline', function (done) {
-      this.browser.wait(webdriver.until.elementLocated(webdriver.By.css('h1 > div')))
-        .then(el => {
-          return el.getText();
-        })
-        .then(txt => {
-          console.log(txt);
-          done();
-        });
+    it('check headline', async () => {
+      // const el = await browser.wait(webdriver.until.elementsLocated(webdriver.By.css('a')));
+      // console.log(el, el.length);
+      const el = await browser.wait(webdriver.until.elementLocated(webdriver.By.css('h1')));
+      const txt = await el.getText();
+      console.log(txt);
     });
   });
 }
